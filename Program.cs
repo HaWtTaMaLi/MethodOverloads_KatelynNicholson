d54d648f13dc6ae56f7daf286ba275bb4e42599e
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-14 15:27:53 -0300

    set up

--- Program.cs
+++ Program.cs
@@ -8,8 +8,36 @@ namespace MethodOverloads_KatelynNicholson
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //
+
+
+        static void Main()
         {
+            //
+
+
+        }
+
+        static void RollDice()
+        {
+            //rolls a 6 sided die and writes to the console
+        }
+        static void RollDice(int sides)
+        {
+            //rolls a single die with a specified amount of sides(i.e a d4, d20, etc)
+            //writes the result to the console
+        }
+        static void RollDice(int sides, int times)
+        {
+            //rolls a die with a specified amount of sides a specified amount of times
+            //writes the result to the console 
+        }
+        static void RollDice(int sides, int times, ConsoleColor color = ConsoleColor.Red)
+        {
+            //sets the console's foreground color to a specified color (the default is red)
+            //rolls a die with a specified amount of sides a specified amount of times
+            //writes the result to the console
+            //sets the foreground color back to white 
         }
     }
 }
